2fe19e839053ffa38b144a4e7c6448458cc4bfce
mung5079 <mung5079@gmail.com>
2024-03-04 15:49:47 +0900

    refactor: useEffect 없이 유효성검사하도록 변경

--- front/src/components/signupform/SignUpForm.tsx
+++ front/src/components/signupform/SignUpForm.tsx
@@ -12,17 +12,14 @@ export const SignUpForm = () => {
   const [isEmailValid, setIsEmailValid] = useState(false);
 
   const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
-    console.log(e);
-    setEmail(e.target.value);
+    const newEmail = e.target.value;
+    setEmail(newEmail);
+    updateEmailValid(newEmail);
   };
-  useEffect(() => {
-    if (!checkEmail(email)) {
-      setIsEmailValid(false);
-    }
-    if (checkEmail(email) || !email) {
-      setIsEmailValid(true);
-    }
-  }, [email]);
+  const updateEmailValid = (newEmail: string) => {
+    if (!checkEmail(newEmail)) setIsEmailValid(false);
+    if (checkEmail(newEmail) || !newEmail) setIsEmailValid(true);
+  };
 
   //패스워드
   const [password, setPassword] = useState("");
@@ -30,16 +27,16 @@ export const SignUpForm = () => {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
   const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
+    const newPassword = e.target.value;
+    setPassword(newPassword);
+    updatePasswordValid(newPassword);
+    updatePasswordConfirmValid(newPassword, passwordConfirm);
   };
-  useEffect(() => {
-    if (!checkPassword(password)) {
-      setIsPasswordValid(false);
-    }
-    if (checkPassword(password) || !password) {
-      setIsPasswordValid(true);
-    }
-  }, [password]);
+  const updatePasswordValid = (newPassword: string) => {
+    if (!checkPassword(newPassword)) setIsPasswordValid(false);
+    if (checkPassword(newPassword) || !newPassword) setIsPasswordValid(true);
+  };
+
   const handlePasswordVisibleClick = () => {
     setIsPasswordVisible(!isPasswordVisible);
   };
@@ -47,23 +44,23 @@ export const SignUpForm = () => {
   //패스워드 확인
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [isPasswordConfirmValid, setIsPasswordConfirmValid] = useState(false);
-  const [isPasswordConfirmVisible, setIsPasswordConfirmVisible] =
-    useState(false);
+  const [isPasswordConfirmVisible, setIsPasswordConfirmVisible] = useState(false);
+
   const handlePasswordConfirmChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setPasswordConfirm(e.target.value);
+    const newPasswordConfirm = e.target.value;
+    setPasswordConfirm(newPasswordConfirm);
+    updatePasswordConfirmValid(password, newPasswordConfirm);
   };
-  useEffect(() => {
-    if (password !== passwordConfirm) {
-      setIsPasswordConfirmValid(false);
-    }
-    if (password === passwordConfirm || !passwordConfirm) {
-      setIsPasswordConfirmValid(true);
-    }
-  }, [password, passwordConfirm]);
+  const updatePasswordConfirmValid = (_password: string, _passwordConfirm: string) => {
+    if (_password !== _passwordConfirm) setIsPasswordConfirmValid(false);
+    if (_password === _passwordConfirm || !_passwordConfirm) setIsPasswordConfirmValid(true);
+  };
+
   const handlePasswordConfirmVisibleClick = () => {
     setIsPasswordConfirmVisible(!isPasswordConfirmVisible);
   };
 
+  //회원가입 버튼
   const [isActive, setIsActive] = useState(false);
 
   useEffect(() => {
@@ -93,45 +90,36 @@ export const SignUpForm = () => {
     <SignUpFormBlock>
       <InputGroup>
         <label htmlFor="email">이메일</label>
-        <input
-          name="email"
-          placeholder="ssafe11@gmail.com"
-          onChange={handleEmailChange}
-        />
-        <span>{!isEmailValid && "유효하지 않은 이메일 형식입니다."}</span>
+        <input id="email" name="email" placeholder="ssafe11@gmail.com" onChange={handleEmailChange} />
+        <span>{email && !isEmailValid && "유효하지 않은 이메일 형식입니다."}</span>
       </InputGroup>
+
       <InputGroup>
         <label htmlFor="password">비밀번호</label>
         <input
+          id="passsword"
           name="password"
           placeholder="비밀번호를 입력하세요."
           type={isPasswordVisible ? "text" : "password"}
           onChange={handlePasswordChange}
         />
-        <img
-          src={isPasswordVisible ? openEye : closeEye}
-          onClick={handlePasswordVisibleClick}
-          alt=""
-        />
-        <span>{!isPasswordValid && "유효하지 않은 비밀번호 형식입니다."}</span>
+        <img src={isPasswordVisible ? openEye : closeEye} onClick={handlePasswordVisibleClick} alt="" />
+        <span>{password && !isPasswordValid && "유효하지 않은 비밀번호 형식입니다."}</span>
       </InputGroup>
+
       <InputGroup>
         <label htmlFor="passwordConfirm">비밀번호 확인</label>
         <input
+          id="passwordConfirm"
           name="passwordConfirm"
           placeholder="비밀번호를 다시 입력해주세요."
           type={isPasswordConfirmVisible ? "text" : "password"}
           onChange={handlePasswordConfirmChange}
         />
-        <img
-          src={isPasswordConfirmVisible ? openEye : closeEye}
-          onClick={handlePasswordConfirmVisibleClick}
-          alt=""
-        />
-        <span>
-          {!isPasswordConfirmValid && "비밀번호가 일치하지 않습니다."}
-        </span>
+        <img src={isPasswordConfirmVisible ? openEye : closeEye} onClick={handlePasswordConfirmVisibleClick} alt="" />
+        <span>{passwordConfirm && !isPasswordConfirmValid && "비밀번호가 일치하지 않습니다."}</span>
       </InputGroup>
+
       <Button active={isActive} onClick={handleBtnClick}>
         회원가입
       </Button>
