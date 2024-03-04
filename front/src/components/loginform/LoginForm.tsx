ce1b28c411034425b8992316f803fe62e9080970
mung96 <mung5079@gmail.com>
2024-03-05 08:57:02 +0900

    feat: 로그인 토큰 로컬스토리지 저장 기능 구현

--- front/src/components/loginform/LoginForm.tsx
+++ front/src/components/loginform/LoginForm.tsx
@@ -1,28 +1,71 @@
 import { LoginFormBlock, InputGroup, Button } from "./LoginForm.element";
 import openEye from "../../assets/openeye.svg";
 import closeEye from "../../assets/closeeye.svg";
-import { useState } from "react";
+import { ChangeEvent, useState, MouseEvent } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const navigator = useNavigate();
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const [password, setPassword] = useState("");
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const handlePasswordVisibleClick = () => {
     setIsPasswordVisible(!isPasswordVisible);
   };
+
+  const [isLogin, setIsLogin] = useState(true);
+  async function handleLoginBtnClick(e: MouseEvent) {
+    e.preventDefault();
+    await axios
+      .post("http://localhost:8000/auth/login", {
+        email: email,
+        pw: password,
+      })
+      .then((res) => {
+        localStorage.setItem("refresh_token", res.data.refreshToken);
+        localStorage.setItem("access_token", res.data.accessToken);
+        navigator("/");
+      })
+      .catch((err) => {
+        setIsLogin(false);
+      });
+  }
+
   return (
     <LoginFormBlock>
       <InputGroup>
         <label htmlFor="email">이메일</label>
-        <input name="email" placeholder="ssafe11@gmail.com" />
-        <span></span>
+        <input
+          name="email"
+          placeholder="ssafe11@gmail.com"
+          onChange={handleEmailChange}
+        />
       </InputGroup>
       <InputGroup>
         <label htmlFor="password">비밀번호</label>
-        <input name="password" placeholder="비밀번호를 입력하세요." type={isPasswordVisible ? "text" : "password"} />
-        <img src={isPasswordVisible ? openEye : closeEye} onClick={handlePasswordVisibleClick} alt="" />
-        <span></span>
+        <input
+          name="password"
+          placeholder="비밀번호를 입력하세요."
+          type={isPasswordVisible ? "text" : "password"}
+          onChange={handlePasswordChange}
+        />
+        <img
+          src={isPasswordVisible ? openEye : closeEye}
+          onClick={handlePasswordVisibleClick}
+          alt=""
+        />
       </InputGroup>
-
-      <Button>로그인</Button>
+      <span>{!isLogin && "이메일 또는 비밀번호가 올바르지 않습니다."}</span>
+      <Button onClick={handleLoginBtnClick}>로그인</Button>
     </LoginFormBlock>
   );
 };
